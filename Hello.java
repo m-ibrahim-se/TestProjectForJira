1ab0a1b0f8cb573c5f78636ae078b3c13f927c7e
Mohammad Ibrahim <65446450+m-ibrahim-se@users.noreply.github.com>
2023-09-19 13:53:01 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
   public static void main(String[] args){
     //Print "Hello World in Java" in the terminal window
     System.out.println("Hello World in Java!!!");
+    System.out.println("I am in the second step!!!");
   }
 }
